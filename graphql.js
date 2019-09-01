fba1779bb957f45a74a78ba5017b1317a5ede623
Robat Williams <robatwilliams@gmail.com>
2019-09-01 16:04:52 +0100

    Add GraphQL hello world using Apollo Lambda

---
+++ graphql.js
@@ -0,0 +1,17 @@
+const { ApolloServer, gql } = require('apollo-server-lambda');
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+exports.graphqlHandler = server.createHandler();
