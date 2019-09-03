60bc035cf4281065dcdd1020b33b684754bf79dd
Robat Williams <robatwilliams@gmail.com>
2019-09-03 22:32:18 +0100

    Mandatory field

--- src/schema.js
+++ src/schema.js
@@ -28,7 +28,7 @@ module.exports = gql`
 
   type Map {
     # Although most are numbers, the 1:25k Outdoor Leisure series are prefixed "OL"
-    sheet: String
+    sheet: String!
   }
 
   enum Scale {
