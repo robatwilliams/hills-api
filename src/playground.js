8795b2fc4410a68924e75af4094eb10b8f664dac
Robat Williams <robatwilliams@gmail.com>
2019-09-22 00:33:58 +0100

    Fix wrong playground relative url when deployed

--- src/playground.js
+++ src/playground.js
@@ -3,7 +3,9 @@ const lambdaPlayground = require('graphql-playground-middleware-lambda').default
 // https://github.com/prisma/graphql-playground/blob/master/packages/graphql-playground-html/src/render-playground-page.ts
 // https://github.com/prisma/graphql-playground#usage
 const options = {
-  endpoint: '../graphql',
+  // Relative to the parent of the playground, not to the playground, so no ../
+  endpoint: 'graphql',
+
   settings: {
     'schema.polling.enable': process.env.IS_OFFLINE === 'true',
 
