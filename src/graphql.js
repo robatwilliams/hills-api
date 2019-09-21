8e779b90d5a9e252b8bbf1086bce76e713ab78b0
Robat Williams <robatwilliams@gmail.com>
2019-09-21 18:30:56 +0100

    Better error message for missing/wrong (curl defaults) content type

--- src/graphql.js
+++ src/graphql.js
@@ -6,6 +6,8 @@ const CountriesDataSource = require('./datasource/CountriesDataSource');
 const HillsDBDataSource = require('./datasource/hillsDB/HillsDBDataSource');
 const { resolvers, schema } = require('./modules');
 
+const REQUEST_MEDIA_TYPES = ['application/json', 'application/graphql'];
+
 const countriesDataSource = new CountriesDataSource();
 const hillsDataSource = new HillsDBDataSource();
 hillsDataSource.start();
@@ -38,8 +40,35 @@ app.use(
 
 const server = awsServerlessExpress.createServer(app);
 
-function handler(event, context) {
-  return awsServerlessExpress.proxy(server, event, context);
+async function handler(event, context) {
+  // async; caller must consistently receive a promise
+
+  return (
+    ensureSupportedContentType(event) ||
+    awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise
+  );
+}
+
+/**
+ * 1. Prevent requests with unsupported types proceeding further
+ * 2. Better status and error message if content type is missing (not 400 "Must provide query string")
+ */
+function ensureSupportedContentType(event) {
+  if (event.httpMethod !== 'POST') {
+    return;
+  }
+
+  // API Gateway normalises to lowercase, serverless-offline does not
+  const contentType = event.headers['content-type'] || event.headers['Content-Type'];
+
+  if (!REQUEST_MEDIA_TYPES.includes(contentType)) {
+    return {
+      statusCode: 415,
+      body: JSON.stringify({
+        errors: [{ message: 'Supports: ' + REQUEST_MEDIA_TYPES.join(', ') }],
+      }),
+    };
+  }
 }
 
 module.exports.fn = handler;
