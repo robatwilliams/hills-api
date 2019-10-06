7b8657a3c73cecf48880493ce14fca51e24eeadf
Robat Williams <robatwilliams@gmail.com>
2019-10-06 12:18:22 +0100

    Factor out common params and promise handling

--- src/datasource/hills/HillsDataSource.js
+++ src/datasource/hills/HillsDataSource.js
@@ -2,13 +2,6 @@ const RDSDataService = require('aws-sdk/clients/rdsdataservice');
 const mapHill = require('./mapHill');
 const { buildParameters, unwrapRecords } = require('./rdsApiUtil');
 
-const staticParams = {
-  database: 'HILLS',
-  includeResultMetadata: true, // include column names
-  resourceArn: process.env.DATABASE_CLUSTER_ARN,
-  secretArn: process.env.DATABASE_CLUSTER_SECRET_ARN,
-};
-
 class HillsDataSource {
   constructor() {
     this.client = new RDSDataService(); // region from env:AWS_REGION
@@ -24,25 +17,23 @@ class HillsDataSource {
     }
 
     const params = {
-      ...staticParams,
       parameters: buildParameters(parameters),
       sql: `SELECT * FROM HILLS ${whereList}`,
     };
 
-    const response = await this.client.executeStatement(params).promise();
+    const response = await this.executeStatement(params);
 
     return unwrapRecords(response).map(mapHill);
   }
 
   async queryOne({ number }) {
     const params = {
-      ...staticParams,
       parameters: buildParameters({ number }),
       sql: 'SELECT * FROM HILLS WHERE number = :number',
     };
 
     // TODO deal with slow-resume
-    const response = await this.client.executeStatement(params).promise();
+    const response = await this.executeStatement(params);
 
     return mapHill(unwrapRecords(response)[0]);
   }
@@ -53,14 +44,25 @@ class HillsDataSource {
     const inList = numbers.join(',');
 
     const params = {
-      ...staticParams,
       parameters: buildParameters({ scale }),
       sql: `SELECT hillNumber, sheet FROM HILLS_MAPS WHERE scale = :scale AND hillNumber IN (${inList})`,
     };
 
-    const response = await this.client.executeStatement(params).promise();
+    const response = await this.executeStatement(params);
 
     return unwrapRecords(response);
+  }
+
+  executeStatement(statementParams) {
+    const params = {
+      database: 'HILLS',
+      includeResultMetadata: true, // include column names
+      resourceArn: process.env.DATABASE_CLUSTER_ARN,
+      secretArn: process.env.DATABASE_CLUSTER_SECRET_ARN,
+      ...statementParams,
+    };
+
+    return this.client.executeStatement(params).promise();
   }
 }
 
