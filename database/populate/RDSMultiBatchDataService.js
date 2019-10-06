4d60ce01554875fbba0f21faafa2315ee7b4d28d
Robat Williams <robatwilliams@gmail.com>
2019-10-06 13:10:35 +0100

    Extract class for multi-batch RDS client

---
+++ database/populate/RDSMultiBatchDataService.js
@@ -0,0 +1,42 @@
+const BATCH_EXECUTE_PARAMETER_SETS_LIMIT = 1000;
+
+class RDSMultiBatchDataService {
+  constructor(client, staticParams) {
+    this.client = client;
+    this.staticParams = staticParams;
+  }
+
+  async batchExecuteStatement(params) {
+    const { parameterSets } = params;
+
+    const batches = chunk(parameterSets, BATCH_EXECUTE_PARAMETER_SETS_LIMIT);
+    console.log(`Inserting ${parameterSets.length} records in ${batches.length} batches`);
+
+    // One at a time, let it stop if anything fails
+    for (const [index, batch] of batches.entries()) {
+      await this._batchExecuteStatement({ ...params, parameterSets: batch });
+      console.log(`Inserted batch ${index + 1}`);
+    }
+
+    console.log('Inserted all batches');
+  }
+
+  _batchExecuteStatement(params) {
+    return this.client
+      .batchExecuteStatement({ ...this.staticParams, ...params })
+      .promise();
+  }
+}
+
+module.exports = RDSMultiBatchDataService;
+
+function chunk(array, length) {
+  const chunks = [];
+
+  for (let startIndex = 0; startIndex < array.length; startIndex += length) {
+    const chunk = array.slice(startIndex, startIndex + length);
+    chunks.push(chunk);
+  }
+
+  return chunks;
+}
--- database/populate/populate.js
+++ database/populate/populate.js
@@ -6,16 +6,21 @@ const RDSDataService = require('aws-sdk/clients/rdsdataservice');
 const csvParse = require('csv-parse/lib/sync');
 const hillsType = require('../ddl/hills');
 const hillsMapsType = require('../ddl/hills_maps');
-const { chunk, getArguments, readFile } = require('../util');
+const { getArguments, readFile } = require('../util');
 const createParameters = require('./createParameters');
 const parseHill = require('./parseHill');
+const RDSMultiBatchDataService = require('./RDSMultiBatchDataService');
 
-const BATCH_EXECUTE_PARAMETER_SETS_LIMIT = 1000;
 const DATA_FILE_PATH = './data/DoBIH_v16_2.csv';
 
 const { region, clusterArn, clusterSecretArn } = getArguments();
 
 const client = new RDSDataService({ region });
+const multiBatchClient = new RDSMultiBatchDataService(client, {
+  database: 'HILLS',
+  resourceArn: clusterArn,
+  secretArn: clusterSecretArn,
+});
 
 (async () => {
   console.log(await main());
@@ -40,7 +45,7 @@ function insertHills(hills) {
   const sql = createInsertStatement('HILLS', hillsType);
   const parameterSets = hills.map(hill => createParameters(hill, hillsType));
 
-  return multiBatchExecuteStatement({ parameterSets, sql });
+  return multiBatchClient.batchExecuteStatement({ parameterSets, sql });
 }
 
 function insertMaps(hills) {
@@ -56,7 +61,7 @@ function insertMaps(hills) {
 
   const parameterSets = entities.map(entity => createParameters(entity, hillsMapsType));
 
-  return multiBatchExecuteStatement({ parameterSets, sql });
+  return multiBatchClient.batchExecuteStatement({ parameterSets, sql });
 }
 
 function createInsertStatement(table, entityType) {
@@ -65,31 +70,6 @@ function createInsertStatement(table, entityType) {
   const parameterList = columns.map(column => ':' + column).join(',');
 
   return `INSERT INTO ${table} (${columnList}) VALUES (${parameterList})`;
-}
-
-async function multiBatchExecuteStatement(params) {
-  const { parameterSets } = params;
-
-  const batches = chunk(parameterSets, BATCH_EXECUTE_PARAMETER_SETS_LIMIT);
-  console.log(`Inserting ${parameterSets.length} records in ${batches.length} batches`);
-
-  // One at a time, let it stop if anything fails
-  for (const [index, batch] of batches.entries()) {
-    await batchExecuteStatement({ ...params, parameterSets: batch });
-    console.log(`Uploaded batch ${index + 1}`);
-  }
-
-  console.log('Uploaded all batches');
-}
-
-function batchExecuteStatement(params) {
-  const staticParams = {
-    database: 'HILLS',
-    resourceArn: clusterArn,
-    secretArn: clusterSecretArn,
-  };
-
-  return client.batchExecuteStatement({ ...staticParams, ...params }).promise();
 }
 
 async function loadData() {
