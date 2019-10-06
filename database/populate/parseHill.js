33d485831d932d58a15efc11b004df6980c47b21
Robat Williams <robatwilliams@gmail.com>
2019-10-06 13:14:14 +0100

    Extract file for map parsing

--- database/populate/parseHill.js
+++ database/populate/parseHill.js
@@ -1,7 +1,5 @@
 const { COUNTRIES_CODES, LIST_CODES } = require('./codes');
-
-const explorerRegex = /^OL\d{1,2}$/;
-const landrangerRegex = /^\d{1,3}$/;
+const parseMaps = require('./parseMaps');
 
 function parseHill(row) {
   const lists = row.Classification.split(',')
@@ -18,35 +16,6 @@ function parseHill(row) {
     name: row.Name,
     number: Number(row.Number),
   };
-}
-
-function parseMaps(string) {
-  if (string === 'IoM OL') {
-    // Isle of Man Leisure map, not Ordnance Survey
-    return string;
-  }
-
-  const maps = string ? string.split(' ') : [];
-
-  return sanitizeMaps(maps);
-}
-
-function sanitizeMaps(maps) {
-  const sanitized = maps.map(sanitizeMap);
-
-  // Remove any duplicates removed by sanitization, e.g. OL39N OL39S
-  return [...new Set(sanitized)];
-}
-
-function sanitizeMap(map) {
-  if (explorerRegex.test(map) || landrangerRegex.test(map)) {
-    return map;
-  } else if (/(N|E|S|W)$/.test(map)) {
-    // Discard any location-on-map information added to the sheet name, e.g. OL47W
-    return sanitizeMap(map.substr(0, map.length - 1));
-  }
-
-  throw new Error('Invalid map sheet: ' + map);
 }
 
 module.exports = parseHill;
---
+++ database/populate/parseMaps.js
@@ -0,0 +1,33 @@
+const explorerRegex = /^OL\d{1,2}$/;
+const landrangerRegex = /^\d{1,3}$/;
+
+function parseMaps(string) {
+  if (string === 'IoM OL') {
+    // Isle of Man Leisure map, not Ordnance Survey
+    return string;
+  }
+
+  const maps = string ? string.split(' ') : [];
+
+  return sanitizeMaps(maps);
+}
+
+function sanitizeMaps(maps) {
+  const sanitized = maps.map(sanitizeMap);
+
+  // Remove any duplicates introduced by sanitization, e.g. OL39N OL39S
+  return [...new Set(sanitized)];
+}
+
+function sanitizeMap(map) {
+  if (explorerRegex.test(map) || landrangerRegex.test(map)) {
+    return map;
+  } else if (/(N|E|S|W)$/.test(map)) {
+    // Discard any location-on-map information added to the sheet name, e.g. OL47W
+    return sanitizeMap(map.substr(0, map.length - 1));
+  }
+
+  throw new Error('Invalid map sheet: ' + map);
+}
+
+module.exports = parseMaps;
