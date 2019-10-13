88605c1ea746988b7dc47f140be9a69047a8c8d0
Robat Williams <robatwilliams@gmail.com>
2019-10-13 02:05:09 +0100

    Bail on many failing integration tests

--- scripts/testIntegration.sh
+++ scripts/testIntegration.sh
@@ -12,12 +12,14 @@ fi
 shift
 
 #
+# bail - many failures likely means Aurora Serverless was sleeping and is taking time to wake up
 # runInBand - run serially, otherwise it'll choke the resource-constrained lambdas and cause errors
 # testEnvironment - doesn't need jsdom
 # $@ - pass remaining arguments on to Jest, e.g. --testRegex=src/integration/http.spec.js
 #
 TEST_INTEGRATION_ROOT_URL=$rootUrl \
   jest \
+  --bail=5 \
   --runInBand \
   --testEnvironment=node \
   ./src/integration \
