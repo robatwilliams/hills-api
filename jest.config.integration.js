4a6fafc4390734c05016df4ab6071d8e0709c7bb
Robat Williams <robatwilliams@gmail.com>
2019-10-16 21:19:42 +0100

    Run integration tests without need for bash script

--- jest.config.integration.js
+++ jest.config.integration.js
@@ -1,8 +1,15 @@
+/**
+ * --runInBand can only be specified on the command line. Used to run the tests
+ * serially, otherwise it'll choke the resource-constrained lambda and cause errors.
+ */
+
 module.exports = {
   // Many failures likely means Aurora Serverless was sleeping and is taking time to wake
   bail: 5,
 
   rootDir: './src/integration',
+
+  setupFiles: ['<rootDir>/jestSetup.js'],
 
   // Doesn't need JSDOM
   testEnvironment: 'node',
---
+++ src/integration/jestSetup.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+const dotenv = require('dotenv');
+
+const result = dotenv.config({
+  path: path.resolve(__dirname, '.env'),
+});
+
+if (result.error) {
+  throw result.error;
+}
