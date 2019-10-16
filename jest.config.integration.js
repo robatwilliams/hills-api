00712e91849da477f94f522c3b45685030691ed3
Robat Williams <robatwilliams@gmail.com>
2019-10-16 20:15:37 +0100

    Pull other settings into integration tests config file

--- jest.config.integration.js
+++ jest.config.integration.js
@@ -1,3 +1,9 @@
 module.exports = {
+  // Many failures likely means Aurora Serverless was sleeping and is taking time to wake
+  bail: 5,
+
   rootDir: './src/integration',
+
+  // Doesn't need JSDOM
+  testEnvironment: 'node',
 };
