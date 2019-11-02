c511f7fe8e9703d40927b80227e37a8a433ffde4
Robat Williams <robatwilliams@gmail.com>
2019-11-02 15:21:16 +0000

    Fix missing NOT NULL constraint, lost in 1c1fbaf

--- database/ddl/HILLS_NAMES.sql
+++ database/ddl/HILLS_NAMES.sql
@@ -1,6 +1,6 @@
 CREATE TABLE HILLS_NAMES (
   hillNumber SMALLINT UNSIGNED NOT NULL,
-  name VARCHAR(100) NULL,
+  name VARCHAR(100) NOT NULL,
   isPrimary boolean NOT NULL,
   PRIMARY KEY (hillNumber, name),
   FOREIGN KEY (hillNumber)
