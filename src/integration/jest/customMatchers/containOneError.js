94e4a643f01f67bf759ed2209c4ce29d429e0150
Robat Williams <robatwilliams@gmail.com>
2019-11-02 23:28:22 +0000

    Subfolder for custom matchers

---
+++ src/integration/jest/customMatchers/containOneError.js
@@ -0,0 +1,22 @@
+/* eslint-disable prefer-template */
+function toContainOneError(response, expectedMessage) {
+  if (this.isNot || this.promise) {
+    throw new Error('Negation and promises not implemented for this matcher');
+  }
+
+  const pass =
+    response.data.errors &&
+    response.data.errors.length === 1 &&
+    response.data.errors[0].message === expectedMessage;
+
+  return {
+    pass,
+    message: () =>
+      this.utils.matcherHint('toContainOneError') +
+      '\n\n' +
+      `Expected one error with message: ${this.utils.printExpected(expectedMessage)}\n` +
+      `Received errors: ${this.utils.printReceived(response.data.errors)}`,
+  };
+}
+
+module.exports = { toContainOneError };
